df5972d625e53fce5163cf8c014a328cacd1374a
Szymon Jeż <jezyk2796@op.pl>
2018-06-20 11:00:06 +0200

    create paddles

--- js/app.js
+++ js/app.js
@@ -11,6 +11,27 @@ const ballSize = 20;
 let ballX = cw /2 - ballSize /2;
 let ballY = ch /2 - ballSize /2;
 
+const paddleHeight = 100;
+const paddleWidth = 20;
+
+const playerX = 70;
+const computerX = 910;
+
+let playerY = 100;
+let computerY = 100;
+
+function player() {
+    //player paddle
+    ctx.fillStyle = '#fc2828';
+    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);
+}
+
+function computer() {
+    //computer paddle
+    ctx.fillStyle = '#000000'
+    ctx.fillRect(computerX, computerY, paddleWidth, paddleHeight);
+}
+
 function ball() {
     //ball
     ctx.fillStyle = '#f1fc28';
@@ -25,3 +46,5 @@ function court() {
 
 court();
 ball();
+player();
+computer();
