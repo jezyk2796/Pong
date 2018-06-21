977d2985edb1473c36183014651a626e949eb6c2
Szymon Jeż <jezyk2796@op.pl>
2018-06-21 15:59:37 +0200

    show points

--- js/app.js
+++ js/app.js
@@ -1,197 +1,216 @@
-const canv = document.querySelector('canvas');
-const ctx = canv.getContext('2d');
+document.addEventListener('DOMContentLoaded', function() {
+    const canv = document.querySelector('canvas');
+    const ctx = canv.getContext('2d');
 
-canv.width = 1000;
-canv.height = 500;
+    canv.width = 1000;
+    canv.height = 500;
 
-const cw = canv.width;
-const ch = canv.height;
+    const cw = canv.width;
+    const ch = canv.height;
 
-const ballSize = 20;
-let ballX = cw /2 - ballSize /2;
-let ballY = ch /2 - ballSize /2;
+    const ballSize = 20;
+    let ballX = cw /2 - ballSize /2;
+    let ballY = ch /2 - ballSize /2;
 
-const paddleHeight = 100;
-const paddleWidth = 20;
+    const paddleHeight = 100;
+    const paddleWidth = 20;
 
-const playerX = 70;
-const computerX = 910;
+    const playerX = 70;
+    const computerX = 910;
 
-let playerY = 200;
-let computerY = 200;
+    let playerY = 200;
+    let computerY = 200;
 
-const netWidth = 6;
-const netHeight = 16;
+    const netWidth = 6;
+    const netHeight = 16;
 
-let ballSpeedX = -3;
-let ballSpeedY = -1;
+    let ballSpeedX = -3;
+    let ballSpeedY = -1;
 
-let leftPoints = 0;
-let rightPoints = 0;
+    let leftPoints = 0;
+    let rightPoints = 0;
 
-function player() {
-    //left player paddle
-    ctx.fillStyle = '#fc2828';
-    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);
-}
+    const leftWin = 'LEFT PLAYER WINS !!!';
+    const rightWin = 'RIGHT PLAYER WINS !!!';
 
-function computer() {
-    //right player paddle
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(computerX, computerY, paddleWidth, paddleHeight);
-}
-
-function ball() {
-    //ball
-    ctx.fillStyle = '#f1fc28';
-    ctx.fillRect(ballX, ballY, ballSize, ballSize);
-
-    //ball movement
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-
-    if (ballY <= 0 || ballY + ballSize >= ch) {
-        ballSpeedY = -ballSpeedY;
+    function player() {
+        //left player paddle
+        ctx.fillStyle = '#fc2828';
+        ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);
     }
 
-    if (ballX >= playerX && ballX <= computerX) {
+    function computer() {
+        //right player paddle
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(computerX, computerY, paddleWidth, paddleHeight);
+    }
 
-        //ball and paddle collision
-        if(ballX < playerX + paddleWidth && ballY + ballSize > playerY && ballY < playerY + paddleHeight) {
-            ballSpeedX = -ballSpeedX;
-            speedUp();
+    function ball() {
+        //ball
+        ctx.fillStyle = '#f1fc28';
+        ctx.fillRect(ballX, ballY, ballSize, ballSize);
+
+        //ball movement
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballY <= 0 || ballY + ballSize >= ch) {
+            ballSpeedY = -ballSpeedY;
         }
-        if(ballX > computerX - paddleWidth && ballY + ballSize > computerY && ballY < computerY + paddleHeight) {
-            ballSpeedX = -ballSpeedX;
-            speedUp();
+
+        if (ballX >= playerX && ballX <= computerX) {
+
+            //ball and paddle collision
+            if(ballX < playerX + paddleWidth && ballY + ballSize > playerY && ballY < playerY + paddleHeight) {
+                ballSpeedX = -ballSpeedX;
+                speedUp();
+            }
+            if(ballX > computerX - paddleWidth && ballY + ballSize > computerY && ballY < computerY + paddleHeight) {
+                ballSpeedX = -ballSpeedX;
+                speedUp();
+            }
+        }
+
+        //counting points
+        if(ballX < 0 - ballSize) {
+            rightPoints += 1;
+        } else if(ballX > cw + ballSize) {
+            leftPoints += 1;
+        }
+
+        //out
+        if(ballX < 0 - ballSize || ballX > cw + ballSize) {
+            ballX = cw /2 - ballSize /2;
+            ballY = ch /2 - ballSize /2;
+
+            if (ballSpeedX > 0) {
+                ballSpeedX = 3;
+                ballSpeedY = 1;
+            } else if (ballSpeedX < 0) {
+                ballSpeedX = -3;
+                ballSpeedY = -1;
+            }
+        }
+
+        //checking points
+        ctx.font = "30px Arial";
+        if(rightPoints === 11) {
+            ctx.strokeText(rightWin, cw/2 + 50, ch/2);
+            clearInterval(screen);
+        } else if (leftPoints === 11) {
+            ctx.strokeText(leftWin, cw/2 - 350, ch/2);
+            clearInterval(screen);
         }
     }
 
-    if(ballX < 0 - ballSize) {
-        rightPoints += 1;
-        console.log(rightPoints);
-    } else if(ballX > cw + ballSize) {
-        leftPoints += 1;
-        console.log(leftPoints);
-    }
+    function speedUp() {
+        //speeding up the ball
 
-    //out
-    if(ballX < 0 - ballSize || ballX > cw + ballSize) {
-        ballX = cw /2 - ballSize /2;
-        ballY = ch /2 - ballSize /2;
+        //speed x
+        if(ballSpeedX > 0 && ballSpeedX <= 20) {
+            ballSpeedX += 0.5;
+        } else if(ballSpeedX < 0 && ballSpeedX >= -20) {
+            ballSpeedX += -0.5;
+        }
 
-        if (ballSpeedX > 0) {
-            ballSpeedX = 3;
-            ballSpeedY = 1;
-        } else if (ballSpeedX < 0) {
-            ballSpeedX = -3;
-            ballSpeedY = -1;
+        //speed y
+        if(ballSpeedY > 0 && ballSpeedY <= 10) {
+            ballSpeedY += 0.5;
+        } else if(ballSpeedY < 0 && ballSpeedY <= -10) {
+            ballSpeedY += -0.5;
         }
     }
-}
 
+    function court() {
+        //court
+        ctx.fillStyle = '#276003';
+        ctx.fillRect(0, 0, cw, ch);
 
-
-// document.getElementById('left').innerText = leftPoints;
-// document.getElementById('right').innerText = rightPoints;
-
-function speedUp() {
-    //speeding up the ball
-
-    //speed x
-    if(ballSpeedX > 0 && ballSpeedX <= 20) {
-        ballSpeedX += 0.5;
-    } else if(ballSpeedX < 0 && ballSpeedX >= -20) {
-        ballSpeedX += -0.5;
+        //net
+        for(let netPosition = 20; netPosition < ch; netPosition += 30) {
+            ctx.fillStyle = '#ffffff';
+            ctx.fillRect(cw /2 - netWidth /2, netPosition, netWidth, netHeight);
+        }
     }
 
-    //speed y
-    if(ballSpeedY > 0 && ballSpeedY <= 10) {
-        ballSpeedY += 0.5;
-    } else if(ballSpeedY < 0 && ballSpeedY <= -10) {
-        ballSpeedY += -0.5;
-    }
-}
+    const move = {
+        player: 'stop',
+        computer: 'stop',
+    };
 
-function court() {
-    //court
-    ctx.fillStyle = '#276003';
-    ctx.fillRect(0, 0, cw, ch);
+    function paddlesMove() {
+        if(move.player === 'up' && playerY > 0) {
+            playerY -= 10;
+        } else if (move.player === 'down' && playerY < 400) {
+            playerY += 10;
+        }
 
-    //net
-    for(let netPosition = 20; netPosition < ch; netPosition += 30) {
-        ctx.fillStyle = '#ffffff';
-        ctx.fillRect(cw /2 - netWidth /2, netPosition, netWidth, netHeight);
-    }
-}
-
-const move = {
-    player: 'stop',
-    computer: 'stop',
-};
-
-function paddlesMove() {
-    if(move.player === 'up' && playerY > 0) {
-        playerY -= 10;
-    } else if (move.player === 'down' && playerY < 400) {
-        playerY += 10;
+        if(move.computer === 'up' && computerY > 0) {
+            computerY -= 10;
+        } else if (move.computer === 'down' && computerY < 400) {
+            computerY += 10;
+        }
     }
 
-    if(move.computer === 'up' && computerY > 0) {
-        computerY -= 10;
-    } else if (move.computer === 'down' && computerY < 400) {
-        computerY += 10;
+    function keyDown(key) {
+        if (key.keyCode == '87') {
+            move.player = 'up';
+            return;
+        }
+        if (key.keyCode == '83') {
+            move.player = 'down';
+            return;
+        }
+        if(key.keyCode == '38') {
+            move.computer = 'up';
+            return;
+        }
+        if(key.keyCode == '40') {
+            move.computer = 'down';
+            return;
+        }
     }
-}
 
-function keyDown(key) {
-    if (key.keyCode == '87') {
-        move.player = 'up';
-        return;
+    function keyUp(key) {
+        if (key.keyCode == '87') {
+            move.player = 'stop';
+            return;
+        }
+        if (key.keyCode == '83') {
+            move.player = 'stop';
+            return;
+        }
+        if(key.keyCode == '38') {
+            move.computer = 'stop';
+            return;
+        }
+        if(key.keyCode == '40') {
+            move.computer = 'stop';
+            return;
+        }
     }
-    if (key.keyCode == '83') {
-        move.player = 'down';
-        return;
-    }
-    if(key.keyCode == '38') {
-        move.computer = 'up';
-        return;
-    }
-    if(key.keyCode == '40') {
-        move.computer = 'down';
-        return;
-    }
-}
 
-function keyUp(key) {
-    if (key.keyCode == '87') {
-        move.player = 'stop';
-        return;
+    function showPoints() {
+        ctx.font = "30px Arial";
+        ctx.strokeText(leftPoints, 30, 50);
+        ctx.strokeText(rightPoints, 950, 50);
     }
-    if (key.keyCode == '83') {
-        move.player = 'stop';
-        return;
-    }
-    if(key.keyCode == '38') {
-        move.computer = 'stop';
-        return;
-    }
-    if(key.keyCode == '40') {
-        move.computer = 'stop';
-        return;
-    }
-}
 
-function game() {
-    paddlesMove();
-    court();
-    ball();
-    player();
-    computer();
-}
+    function game() {
+        paddlesMove();
+        court();
+        ball();
+        player();
+        computer();
+        showPoints();
+    }
 
 
-setInterval(game, 1000 / 60);
+    const screen = setInterval(game, 1000 / 60);
 
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
+    document.addEventListener('keydown', keyDown);
+    document.addEventListener('keyup', keyUp);
+});
+
+
+
